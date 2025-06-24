8b25cdec641863fe7cfaedb6fc80653f2a9871c4
Callum <callumery11@outlook.com>
2025-06-24 12:15:02 +0100

    Defined method on type Node

--- b-tree.go
+++ b-tree.go
@@ -11,10 +11,13 @@ type Node struct {
 	isRoot   bool
 }
 
-func insert(node *Node, value int) {
-	if node == nil {
-		return
-	}
+func (n *Node) insert(value int) {
+	//Each node must have AT LEAST 2 nodes.
+	if n.isRoot {
+		if n.root[0] == nil {
+			n.root[0] = &value //Start node can have one element.
+			return
+		}
 
 	var rootLen = len(node.root)
 	fmt.Printf("Node size: %d\n", rootLen)
